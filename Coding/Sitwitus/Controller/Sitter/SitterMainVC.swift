66f743f88873e3b4651b772adbeab78696867ea1
ShahRukh <shahaider@gmail.com>
2020-01-30 21:22:48 +0500

    custom marker snippet

--- Coding/Sitwitus/Controller/Sitter/SitterMainVC.swift
+++ Coding/Sitwitus/Controller/Sitter/SitterMainVC.swift
@@ -19,7 +19,11 @@ class SitterMainVC: UIViewController {
 
                                    //******** VARIABLES *************
      var mapView : GMSMapView?
+     var tappedMarker = GMSMarker()
      
+     
+//     var infoWindow = MapMarkerView(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
+     var infoWindow = MapMarkerView()
                                    
                                    //********* FUNCTIONS ***************
     
@@ -49,7 +53,9 @@ class SitterMainVC: UIViewController {
           
           let camera = GMSCameraPosition.camera(withLatitude: 34.0522, longitude: -118.2437, zoom: 10.0)
           self.mapView = GMSMapView.map(withFrame: localMapView.bounds, camera: camera)
+          self.mapView?.delegate = self
             self.localMapView.addSubview(mapView!)
+//          view = mapView
      }
 //*************** GOOGLE MARKER
      
@@ -72,3 +78,46 @@ class SitterMainVC: UIViewController {
 
 
                                       //*************** EXTENSION ******************
+
+extension SitterMainVC: GMSMapViewDelegate{
+     
+     
+     class func instanceFromNib() -> MapMarkerView {
+     return UINib(nibName: "MapMarkerView", bundle: nil).instantiate(withOwner: nil, options: nil)[0] as! MapMarkerView    }
+     
+     func mapView(_ mapView: GMSMapView, didTap marker: GMSMarker) -> Bool {
+          
+          let location = CLLocationCoordinate2D(latitude: marker.position.latitude, longitude: marker.position.longitude)
+          tappedMarker = marker
+          infoWindow.removeFromSuperview()
+          
+          infoWindow = SitterMainVC.instanceFromNib()
+          infoWindow.personName.text = "shahrukh"
+          infoWindow.personAddress.text = "Karachi"
+//          infoWindow.bookButton.addTarget(Any?, action: <#T##Selector#>, for: <#T##UIControl.Event#>)
+          infoWindow.center = mapView.projection.point(for: location)
+          infoWindow.center.y = infoWindow.center.y + 100
+          infoWindow.frame.size.width = 250
+          infoWindow.frame.size.height = 150
+//          
+          self.view.addSubview(infoWindow)
+          return false
+
+     }
+     
+     
+     
+      func mapView(_ mapView: GMSMapView, markerInfoWindow marker: GMSMarker) -> UIView? {
+             return UIView()
+     }
+
+         func mapView(_ mapView: GMSMapView, didChange position: GMSCameraPosition) {
+          self.view.bringSubviewToFront(localMapView)
+                 let location = CLLocationCoordinate2D(latitude: tappedMarker.position.latitude, longitude: tappedMarker.position.longitude)
+                 infoWindow.center = mapView.projection.point(for: location)
+         }
+         
+         func mapView(_ mapView: GMSMapView, didTapAt coordinate: CLLocationCoordinate2D) {
+             infoWindow.removeFromSuperview()    }
+     
+}
