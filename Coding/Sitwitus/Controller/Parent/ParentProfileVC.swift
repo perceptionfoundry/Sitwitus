dd13f007ef3eff7f1bc6cf8efb1a803e4bc92eeb
ShahRukh <shahaider@gmail.com>
2020-02-13 12:22:27 +0500

    Profile VC DONE

---
+++ Coding/Sitwitus/Controller/Parent/ParentProfileVC.swift
@@ -0,0 +1,49 @@
+//
+//  ParentProfileVC.swift
+//  Sitwitus
+//
+//  Created by Syed ShahRukh Haider on 13/02/2020.
+//  Copyright © 2020 Syed ShahRukh Haider. All rights reserved.
+//
+
+import UIKit
+
+class ParentProfileVC: UIViewController {
+
+
+                                    //******** OUTLETS ***************
+
+
+
+                                   //******** VARIABLES *************
+
+                                   
+                                   //********* FUNCTIONS ***************
+    
+    
+//******* VIEW FUNCTIONS
+
+ override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+  override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+
+
+
+                                    //*************** OUTLET ACTION ******************
+
+     @IBAction func backButton(){
+          self.navigationController?.popViewController(animated: true)
+     }
+
+}
+
+
+
+
+                                      //*************** EXTENSION ******************
